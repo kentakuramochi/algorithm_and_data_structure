f54c7a272399fad3c1f7a337203d065c377d73ab
kentakuramochi <soratobi93@gmail.com>
2018-10-29 23:57:34 +0900

    comb sort, insertion sort

---
+++ 01_sort/combsort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <time.h>
+#include "utils.h"
+
+// comb sort
+void combSort(int *data, int n, double k)
+{
+    int tmp, flag, gap;
+    // a gap between 2 compared elements
+    gap = n;
+
+    do {
+        // the gap is divided by k (shrink factor)
+        gap = (int)(gap / k);
+
+        if (gap == 0) {
+            gap = 1;
+        }
+
+        flag = 1;
+        for (int i = 0; i < (n - gap); i++) {
+            // swap
+            if (data[i] > data[i + gap]) {
+                flag = 0;
+                tmp = data[i];
+                data[i] = data[i + gap];
+                data[i + gap] = tmp;
+            }
+        }
+    // if gap = 1, it equivalent to bubble sort
+    } while ((gap > 1) || (flag != 1));
+}
+
+int main()
+{
+    const int N = 10000;
+    int array[N];
+
+    clock_t start, end;
+
+    fill_random(array, N);
+
+    START(start);
+
+    combSort(array, N, 1.3);
+
+    FINISH(end);
+
+    print_all(array, N);
+
+    printf("elapsed: %f[s]\n", ELAPSED_SEC(start, end));
+
+    return 0;
+}
---
+++ 01_sort/insertionsort.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <time.h>
+#include "utils.h"
+
+// insertion sort
+void insertionSort(int *data, int n)
+{
+    int i, sorted, insert;
+
+    for (sorted = 0; sorted < (n - 1); sorted++) {
+        // get next value of sorted list
+        insert = data[sorted++];
+
+        // search a position to insert
+        for (i = 0; i <= sorted; i++) {
+            if (data[i] > insert) {
+                break;
+            }
+        }
+
+        // insert and shift
+        while (i <= (sorted + 1)) {
+            int tmp = data[i];
+            data[i] = insert;
+            insert = tmp;
+            i++;
+        }
+    }
+}
+
+int main()
+{
+    const int N = 10000;
+    int array[N];
+
+    clock_t start, end;
+
+    fill_random(array, N);
+
+    START(start);
+
+    insertionSort(array, N);
+
+    FINISH(end);
+
+    print_all(array, N);
+
+    printf("elapsed: %f[s]\n", ELAPSED_SEC(start, end));
+
+    return 0;
+}
