2a2c684fb825a57e9c900ad58745ac805299c234
kentakuramochi <soratobi93@gmail.com>
2018-11-27 22:18:22 +0900

    display shortest paths

--- 13_graph/dijkstra.c
+++ 13_graph/dijkstra.c
@@ -2,18 +2,30 @@
 
 #define STATION_NUMBER 6
 #define START_STATION 0
+#define GOAL_STATION 5
+
+// node
+typedef struct TagNode {
+    char *name;
+    int *weights;           // weights with connected nodes
+    int cost;               // cost (required time)
+    int fixed;              // fixed flag
+    struct TagNode *prev;   // previous node
+} Node;
+
+
+Node nodes[STATION_NUMBER];
+
+// required time from start station
+//int current_cost[STATION_NUMBER];
+// fixed flag
+//int fixed[STATION_NUMBER];
 
 char *stations[] = {
     "Yokohama", "Musashikosugi", "Shinagawa", "Shibuya", "Shinbashi", "Tameike-Sanno"
 };
 
-// required time from start station
-int current_cost[STATION_NUMBER];
-// fixed flag
-int fixed[STATION_NUMBER];
-
-// weighted graph
-// weight: required time
+// adjacendy matrix
 int adjacency_matrix[STATION_NUMBER][STATION_NUMBER] = {
     { 0, 12, 28, 0, 0, 0 },
     { 12, 0, 10, 13, 0, 0 },
@@ -23,61 +35,82 @@ int adjacency_matrix[STATION_NUMBER][STATION_NUMBER] = {
     { 0, 0, 0, 9, 4, 0 }
 };
 
-// Dijkstra's algorithm
-void dijkstra()
+void init_nodes()
 {
-    int i, min_station, min_time, new_time;
+    int i;
+    for (i = 0; i < STATION_NUMBER; i++) {
+        nodes[i].name = stations[i];
+        nodes[i].weights = adjacency_matrix[i];
+        nodes[i].cost = -1;
+        nodes[i].fixed = 0;
+        nodes[i].prev = NULL;
+    }
+}
+
+// Dijkstra's algorithm
+void dijkstra(int start, int goal)
+{
+    Node *nearest;
+    int i, new_cost;
 
     // init
-    for (i = 0; i < STATION_NUMBER; i++) {
-        current_cost[i] = -1;
-        fixed[i] = 0;
-    }
+    init_nodes();
 
     // cost of start station: 0
-    current_cost[0] = 0;
+    nodes[start].cost = 0;
 
     while (1) {
-        min_time = -1;
+        nearest = NULL;
         for (i = 0; i < STATION_NUMBER; i++) {
             // check station which required time is not fixed
-            if (fixed[i] == 0 && current_cost[i] != -1) {
-                if (min_time == -1 || min_time > current_cost[i]) {
-                    min_time = current_cost[i];
-                    min_station = i;
+            if (nodes[i].fixed == 0 && nodes[i].cost != -1) {
+                if (nearest == NULL || nearest->cost > nodes[i].cost) {
+                    nearest = &nodes[i];
                 }
             }
         }
 
         // all required time is fixed
-        if (min_time == -1) {
+        if (nearest == NULL) {
             break;
         }
 
-        // check required time from this station to all other stations
         for (i = 0; i < STATION_NUMBER; i++) {
-            if (fixed[i] == 0 && adjacency_matrix[min_station][i] > 0) {
+            if (nearest->fixed == 0 && nearest->weights[i] > 0) {
                 // required time via this station
-                new_time = min_time + adjacency_matrix[min_station][i];
+                new_cost = nearest->cost + nearest->weights[i];
 
                 // if the required time is shorter than registered time, renew time
-                if (current_cost[i] == -1 || current_cost[i] > new_time) {
-                    current_cost[i] = new_time;
+                if (nodes[i].cost == -1 || nodes[i].cost > new_cost) {
+                    nodes[i].cost = new_cost;
+                    nodes[i].prev = nearest;
                 }
             }
         }
 
         // fix
-        fixed[min_station] = 1;
+        nearest->fixed = 1;
     }
 }
 
 int main(void)
 {
-    dijkstra();
+    Node *station;
+
+    dijkstra(START_STATION, GOAL_STATION);
+
+    station = &nodes[GOAL_STATION];
 
     // required time from Yokohama to Tameike-Sanno
-    printf("%s -> %s (%d min)\n", stations[0], stations[5], current_cost[5]);
+    while (1) {
+        printf("%s (%d min)", station->name, station->cost);
+        if (station->prev == NULL) {
+            break;
+        }
+        station = station->prev;
+        printf(" <- ");
+    }
+    printf("\n");
 
     return 0;
 }
